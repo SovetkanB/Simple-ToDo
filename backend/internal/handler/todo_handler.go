a237ea566e3908b9b5d9e12bf5e80569364c6553
purelple <elnazkarimova_2004@mail.ru>
2026-02-22 15:52:41 +0500

    added updated and delete handler

--- backend/internal/handler/todo_handler.go
+++ backend/internal/handler/todo_handler.go
@@ -7,6 +7,7 @@ import (
 	"simple-todo/helpers"
 	"simple-todo/internal/models"
 	"simple-todo/internal/service"
+	"strings"
 )
 
 type TodoHandler struct {
@@ -45,8 +46,37 @@ func (h *TodoHandler) GetAllTodos(w http.ResponseWriter, r *http.Request) {
 	helpers.ResponseJson(w, http.StatusOK, todos)
 }
 
-func (h *TodoHandler) UpdateTodo(w http.ResponseWriter, r *http.Request) {
+func (h *TodoHandler) UpdateTodo(w http.ResponseWriter, r *http.Request, id string) {
+	var todo models.ToDo
+	if err := json.NewDecoder(r.Body).Decode(&todo); err != nil {
+		slog.Error("Failed to decode todo request", "error", err.Error())
+		helpers.ResponseError(w, http.StatusBadRequest, "Invalid request body")
+		return
+	}
+
+	todo.ID = id
+
+	if err := h.todoService.UpdateTodo(&todo); err != nil {
+		if strings.Contains(err.Error(), "not found") {
+			helpers.ResponseError(w, http.StatusNotFound, err.Error())
+		} else {
+			helpers.ResponseError(w, http.StatusBadRequest, err.Error())
+		}
+		return
+	}
+
+	helpers.ResponseJson(w, http.StatusOK, todo)
 }
 
-func (h *TodoHandler) DeleteTodo(w http.ResponseWriter, r *http.Request) {
+func (h *TodoHandler) DeleteTodo(w http.ResponseWriter, r *http.Request, id string) {
+	if err := h.todoService.DeleteTodo(id); err != nil {
+		if strings.Contains(err.Error(), "not found") {
+			helpers.ResponseError(w, http.StatusNotFound, err.Error())
+		} else {
+			helpers.ResponseError(w, http.StatusBadRequest, "Failed to delete todo")
+		}
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 }
